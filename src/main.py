4fab141116ce5e0c37fde964a9d92677fcb8d139
Fat Tiger <w1752738919@gmail.com>
2019-12-19 23:52:51 +0800

    add func

--- src/main.py
+++ src/main.py
@@ -1,17 +1,30 @@
-import numpy as np
 import cv2 as cv
+import face_recognition
 
 cap = cv.VideoCapture('rtsp://player.daniulive.com:554/live20656.sdp')
+my_face_encodings = face_recognition.load_image_file("./source/wangshenjie_idcard.jpg")
 
 while cap.isOpened():
+    print('Reading frame ...')
     ret, frame = cap.read()
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
+    print('Read frame success')
+    print('Face encoding ...')
+    face_encodings = face_recognition.face_encodings(image)
+    print('Face encoded')
+    print('Face quantity ' + face_encodings.length)
+    for face_encoding in face_encodings:
+        compare_flag = False
+        print('Face comparing ...')
+        compare_results = face_recognition.compare_faces(my_face_encodings, face_encoding)
+        for compare_result in compare_results:
+            if compare_result == True:
+                compare_flag = True
+                print('Face recognition success')
+                break
+        if compare_flag == True:
+            break
 cap.release()
-cv.destroyAllWindows()
