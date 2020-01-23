49f7e9724d9acfdd58c6c79e865a0bd0e083b898
Fat Tiger <w1752738919@gmail.com>
2020-01-23 23:26:06 +0800

    initial commit'

--- src/main.py
+++ src/main.py
@@ -1,42 +1,45 @@
 import os
+import time
 import sys
-import _thread as thread
+import queue
 import cv2 as cv
 import numpy as np
 import face_recognition
+from multiprocessing import Process
+import multiprocessing
 
-frame = None
-lock = False
-
-def update_frame(rtsp_url):
+def update_frame(rtsp_url,q):
     print('Connecting to video ...')
     # set rtsp protocol path
     cap = cv.VideoCapture(rtsp_url)
     print('Connected')
     while cap.isOpened():
-        print('Reading frame ...')
+        # print('Reading frame ...')
         ret, current_frame = cap.read()
         # if frame is read correctly ret is True
         if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        if lock == True:
+            print('Can\'t receive frame (stream end?). Exiting ...')
             continue
-        frame = current_frame
+        if not q.empty():
+            try:
+                q.get_nowait()   # discard previous (unprocessed) frame
+            except queue.Empty:
+                pass
+        q.put(current_frame)
     cap.release()
 
-def main():
-    thread.start_new_thread(update_frame, (sys.argv[1],) )
+def main(q):
     source_path = sys.argv[2]
     source_face_encodings = []
     # cache source face encoding to memory
     for file_name in os.listdir(source_path):
         file_path = source_path + file_name
         source_face_encodings += face_recognition.face_encodings(face_recognition.load_image_file(file_path))
+    print('source face encoded')
     while True:
-        lock = True
+        frame = q.get()
+        print('Get frame')
         face_encodings = face_recognition.face_encodings(frame)
-        lock = False
         print('Face encoded')
         print(face_encodings)
         for face_encoding in face_encodings:
@@ -47,4 +50,7 @@ def main():
                 break
 
 if __name__ == "__main__":
-    main()
+    _queue = multiprocessing.Queue()
+    p = Process(target=update_frame, args=(sys.argv[1],_queue,))
+    p.start()
+    main(_queue)
