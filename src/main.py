1b41649efa25ad7ab46234429a5ee3744d7925ea
Fat Tiger <w1752738919@gmail.com>
2020-02-13 00:22:26 +0800

    use gpiozero

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import os
 import time
 import sys
-import queue
 import cv2 as cv
 import numpy as np
 import face_recognition
 import multiprocessing
+import gpiozero
 
 def updateframe(rtsp_url,lock):
     print('connecting to video ...')
@@ -17,26 +17,31 @@ def updateframe(rtsp_url,lock):
         ret, frame = cap.read()
         # if frame is read correctly ret is True
         if not ret:
-            print('can\'t receive frame (stream end?). Exiting ...')
+            print('can\'t receive frame (stream end?). exiting ...')
             continue
         lock.acquire()
         cv.imwrite('./temp.jpg', frame)
         lock.release()
+    print('connection error. exiting ...')
     cap.release()
 
 def main(lock,source_face_encodings):
+    gpio21 = gpiozero.LED(21)
     while True:
         lock.acquire()
         frame = face_recognition.load_image_file('./temp.jpg')
         lock.release()
-        print('loaded image')
         face_encodings = face_recognition.face_encodings(frame)
-        print('face encoded')
+        # print('Face encoded')
         for face_encoding in face_encodings:
             print('face comparing ...')
             compare_results = face_recognition.compare_faces(source_face_encodings, face_encoding)
             if np.any(compare_results) == True:
                 print('face recognition success')
+                gpio21.on()
+                print('lock opened')
+                time.sleep(2)
+                print('lock closed')
                 break
 
 if __name__ == "__main__":
